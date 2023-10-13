8e27328cb795bbd64aa4a609d1659416850d05ca
Felixander Kery <fkery@student04.cse.nd.edu>
2023-10-13 16:27:03 -0400

    Nav Bar Links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,17 @@
-// header function that inputs header html
+import { Link } from "react-router-dom";
+// header function that inputs header nav html
 const Header = () => {
     return (
       <div className="nav_div">
         <ul className="navigation">
           <li className="name">
             <a href=".">Scholar Suppers</a>
+          </li>
+          <li>
+            <Link to="/Main">Groups</Link>
+          </li>
+          <li>
+            <Link to="/GroupHome">Meals</Link>
           </li>
           <li>
             <a href="../index.html">Log out</a>
--- src/Components/components.js
+++ src/Components/components.js
@@ -2,10 +2,11 @@ import Main from "./Main/Main";
 import GroupHome from "./GroupHome/GroupHome";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// routing for which links take to which pages 
+// root will be changed in upcoming feature to sign in page
 export default function Components() {
   return (
     <Router>
-      <Main />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Main" element={<Main />} />
