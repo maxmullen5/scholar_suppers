1d3269106bf3c1a57de3c13a263adaf374ee8784
Max Mullen <mmullen@jisekihealth.com>
2023-11-05 17:54:36 -0500

    Imported fonts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import Parse from "parse";
 import './App.css';
 import Components from "./Components/components";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:400,700', 'Ubuntu:500,700']
+  }
+});
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 Parse.serverURL = Env.SERVER_URL;
