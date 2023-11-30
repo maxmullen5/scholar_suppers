984f43b92299601d5abe8e2989f154c098b35a14
Felixander Kery <fkery@student04.cse.nd.edu>
2023-11-29 20:57:50 -0500

    Add Group Modal

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getAllGroups } from "../../Common/Services/Groups";
+import { getAllGroups, createGroup } from "../../Common/Services/Groups";
 import { getCurrentUserId, getCurrentUserName } from "../../Common/Services/AuthService";
 import Header from "../Header/Header.js";
 import MainList from "./MainList";
@@ -7,6 +7,9 @@ import MainList from "./MainList";
 const Main = () => {
   const [groups, setGroups] = useState([]);
   const [userName, setUserName] = useState(''); // State to store the user's name
+  const [showAddGroupForm, setShowAddGroupForm] = useState(false);
+  const [newGroupName, setNewGroupName] = useState('');
+  const [newGroupPassword, setNewGroupPassword] = useState('');
 
   useEffect(() => {
     const userId = getCurrentUserId();
@@ -27,6 +30,44 @@ const Main = () => {
     }
   }, []);
 
+  /*const handleAddGroup = async () => {
+    const result = await createGroup(newGroupName, newGroupPassword);
+    if (result.success) {
+      setGroups([...groups, { id: result.groupId, name: newGroupName }]);
+      setShowAddGroupModal(false); // Close the modal
+      // Reset the form fields
+      setNewGroupName('');
+      setNewGroupPassword('');
+    } else {
+      // Handle errors and display a notification
+      console.log("Error adding group")
+    }
+  };*/
+
+  const handleButtonClick = async () => {
+    if (!showAddGroupForm) {
+      setShowAddGroupForm(true); // First click: Show the form
+    } else {
+      // Second click: Submit the form
+      const result = await createGroup(newGroupName, newGroupPassword);
+      if (result.success) {
+        // Update groups state with the new group
+        setGroups([...groups, { id: result.groupId, name: newGroupName }]);
+
+        // Reset form state
+        setNewGroupName('');
+        setNewGroupPassword('');
+        setShowAddGroupForm(false);
+
+        // Show success message to the user
+        alert("Group created successfully!");
+      } else {
+        // Handle errors
+        alert("Failed to create group. Please try again.");
+      }
+    }
+  };
+
   return (
     <div>
       <Header />
@@ -36,6 +77,14 @@ const Main = () => {
           <h2>Your Groups:</h2>
         </div>
         <MainList groups={groups} />
+        <button onClick={handleButtonClick}>{showAddGroupForm ? "Create Group" : "Add Group"}</button>
+        {showAddGroupForm && (
+          <div className="modal">
+            <input type="text" placeholder="Group Name" value={newGroupName} onChange={(e) => setNewGroupName(e.target.value)} />
+            <input type="password" placeholder="Password" value={newGroupPassword} onChange={(e) => setNewGroupPassword(e.target.value)} />
+          </div>
+        )}
+
       </div>
     </div>
   );
